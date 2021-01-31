4ce6804dd9800f3770e57050c6b8ae6c425e5c42
Napoleon Stanley <napoleonstanley316@gmail.com>
2021-01-30 19:52:38 -0600

    get route for genres is returninig all genres

---
+++ TODO.md
@@ -0,0 +1,6 @@
+
+
+1. [] add genre selection menu
+ - look at adding check boxes for each genre
+ 
+2. [] create "add genres" button
--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  // get all genres and order by id
+  const queryText = `SELECT * FROM genres ORDER BY "id" ASC`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error on query ${error}`);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
